a518d8ab636fb4eaa1c61612fbf5b295bc781c1a
Alison Felizzi <alison.felizzi@mongodb.com>
2021-03-18 06:10:48 +0000

    wip cmake gcc version check

--- configs/power8/linux/plat_gcc.cmake
+++ configs/power8/linux/plat_gcc.cmake
@@ -9,20 +9,3 @@
 cmake_minimum_required(VERSION 3.11.0)
 
 set(CROSS_COMPILER_PREFIX "powerpc64-linux-gnu-" CACHE INTERNAL "" FORCE)
-
-# XXX: Need to extract gcc version, currently assumed >= 7
-
-string(APPEND gcc_base_c_flags " -Wformat-signedness")
-string(APPEND gcc_base_c_flags " -Wjump-misses-init")
-string(APPEND gcc_base_c_flags " -Wredundant-decls")
-string(APPEND gcc_base_c_flags " -Wunused-macros")
-string(APPEND gcc_base_c_flags " -Wvariadic-macros")
-string(APPEND gcc_base_c_flags " -Wduplicated-cond")
-string(APPEND gcc_base_c_flags " -Wlogical-op")
-string(APPEND gcc_base_c_flags " -Wunused-const-variable=2")
-string(APPEND gcc_base_c_flags " -Walloca")
-string(APPEND gcc_base_c_flags " -Walloc-zero")
-string(APPEND gcc_base_c_flags " -Wduplicated-branches")
-string(APPEND gcc_base_c_flags " -Wformat-overflow=2")
-string(APPEND gcc_base_c_flags " -Wformat-truncation=2")
-string(APPEND gcc_base_c_flags " -Wrestrict")
