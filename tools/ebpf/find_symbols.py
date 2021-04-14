f77ae6893798055e4af95b24e2cdf659d210f816
Alex Cameron <alex.cameron@10gen.com>
2021-04-14 06:56:57 +0000

    Add find symbols script

---
+++ tools/ebpf/find_symbols.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from bcc import BPF
+import argparse
+
+def find_symbols(wt_lib):
+    user_functions = BPF.get_user_functions(
+        wt_lib, str())
+    for f in user_functions:
+        print(f)
+
+parser = argparse.ArgumentParser(
+    description="Find WiredTiger symbols with eBPF",
+    formatter_class=argparse.RawDescriptionHelpFormatter)
+
+parser.add_argument(
+    "-l", "--lib", type=str, dest="wt_lib",
+    help="WiredTiger library path",
+    required=True)
+args = parser.parse_args()
+
+find_symbols(args.wt_lib)
