ce70df7c08ae789e7391a8ae0cb300893f13acbd
Alex Cameron <alex.cameron@10gen.com>
2021-04-14 04:05:09 +0000

    Only track one stat at a time

--- tools/ebpf/wtebpf.py
+++ tools/ebpf/wtebpf.py
@@ -13,11 +13,12 @@ parser = argparse.ArgumentParser(
     description="Trace WiredTiger with eBPF",
     formatter_class=argparse.RawDescriptionHelpFormatter)
 
+supported_stats = ['frequency','latency','stack']
+
 parser.add_argument(metavar="function", nargs="+", dest="functions", help="function(s) to trace")
 parser.add_argument("-l", "--lib", type=str, dest="wt_lib", help="WiredTiger library path", required=True)
+parser.add_argument("-s", "--stat", choices=supported_stats, dest="stat", help="stat to track", required=True)
 args = parser.parse_args()
-
-supported_stats = ['frequency','latency','stack']
 
 def latency_thread(functions, wt_lib, event):
     print("Starting latency stat thread for functions [%s]" % functions)
@@ -26,42 +27,16 @@ def latency_thread(functions, wt_lib, event):
     calculate_latencies(functions[0])
     print("latency stat thread exiting")
 
-stat_config = {}
-for func in args.functions:
-    # function_name:frequency,latency,stack
-    func_items = func.split(":")
-    if len(func_items) != 2:
-        print('Invalid format: %s' % func)
-        print('Usage: function_name:frequency,latency,stack')
-        exit(1)
-    stat_items = func_items[1].split(',')
-    if len(stat_items) == 0:
-        print('Invalid Format - function requires statistic: %s' % func)
-        print('Usage: function_name:frequency,latency,stack')
-        exit(1)
-    if not all(i in supported_stats for i in stat_items):
-        print('Invalid Format - Invalid statistic specified: %s' % func)
-        print('Usage: function_name:frequency,latency,stack')
-        exit(1)
-
-    for stat in stat_items:
-        if stat not in stat_config:
-            stat_config[stat] = []
-        stat_config[stat].append(func_items[0])
-
-stat_threads = []
 exit_event = threading.Event()
-for stat,functions in stat_config.items():
-    target_function = None
-    if stat == 'frequency':
-        target_function = frequency_thread
-    elif stat == 'latency':
-        target_function = latency_thread
-    else:
-        target_function = stat_stacktrace.stackTraceThread
-    stat_thread = threading.Thread(target=target_function, args=(functions,args.wt_lib,exit_event,))
-    stat_threads.append(stat_thread)
-    stat_thread.start()
+target_function = None
+if args.stat == 'frequency':
+    target_function = frequency_thread
+elif args.stat == 'latency':
+    target_function = latency_thread
+else:
+    target_function = stat_stacktrace.stackTraceThread
+stat_thread = threading.Thread(target=target_function, args=(args.functions,args.wt_lib,exit_event,))
+stat_thread.start()
 
 try:
     while True:
@@ -69,5 +44,4 @@ try:
 except KeyboardInterrupt:
     exit_event.set()
 
-for thread in stat_threads:
-    thread.join()
+stat_thread.join()
